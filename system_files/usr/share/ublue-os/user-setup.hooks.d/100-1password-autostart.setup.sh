2654d4836a1a33b049533797451fefb245e430a8
Jordan Thirus <jordan.thirus@gmail.com>
2025-08-31 11:55:53 -0400

    fix: move 1password autostart script

--- system_files/usr/share/ublue-os/user-setup.hooks.d/100-1password-autostart.setup.sh
+++ system_files/usr/share/ublue-os/user-setup.hooks.d/100-1password-autostart.setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
+source /usr/lib/ublue/setup-services/libsetup.sh
+version-script azem-os-1password user 1 || exit 0
+
 AUTOSTART_DIR=.config/autostart
 
-# Setup VSCode
 if test ! -e "$HOME"/"$AUTOSTART_DIR"/1password.desktop; then
 	mkdir -p "$HOME"/"$AUTOSTART_DIR"
 	cp -f /etc/skel/"$AUTOSTART_DIR"/1password.desktop "$HOME"/"$AUTOSTART_DIR"/1password.desktop
