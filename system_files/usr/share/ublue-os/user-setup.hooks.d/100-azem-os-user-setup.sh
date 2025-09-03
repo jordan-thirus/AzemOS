2c605e69f167ffb94ab21bd84cd1e713bd9a0746
Jordan Thirus <jordan.thirus@gmail.com>
2025-09-03 19:41:46 -0400

    fix: update 1password user setup scripts - do post-install for cli in a privileged hook

--- system_files/usr/share/ublue-os/user-setup.hooks.d/100-azem-os-user-setup.sh
+++ system_files/usr/share/ublue-os/user-setup.hooks.d/100-azem-os-user-setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 source /usr/lib/ublue/setup-services/libsetup.sh
-version-script azem-os-1password user 1 || exit 0
+version-script azem-os-user-setup user 1 || exit 0
 
+# Copy 1password autostart configuration
 AUTOSTART_DIR=.config/autostart
-
 if test ! -e "$HOME"/"$AUTOSTART_DIR"/1password.desktop; then
 	mkdir -p "$HOME"/"$AUTOSTART_DIR"
 	cp -f /etc/skel/"$AUTOSTART_DIR"/1password.desktop "$HOME"/"$AUTOSTART_DIR"/1password.desktop
