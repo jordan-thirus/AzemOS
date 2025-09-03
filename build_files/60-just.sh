314ff405f5053b48a460806fafe6370329777ea4
Jordan Thirus <jordan.thirus@gmail.com>
2025-09-03 19:40:23 -0400

    fix: justfile spec paths - remove duplicate logs

--- build_files/60-just.sh
+++ build_files/60-just.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-echo "=== Adding ujust commands ==="
-
 # Add AzemOS just file
 echo "import \"/usr/share/azem-os/just/95-azem-os.just\"" >>/usr/share/ublue-os/justfile
